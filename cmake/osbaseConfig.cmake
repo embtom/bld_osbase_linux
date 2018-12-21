ba32be5e92e4da23dba28cc4aeb7263606dd1848
tom3333 <t.willetal@googlemail.com>
2018-12-21 22:43:49 +0100

    Update subprojects

--- cmake/osbaseConfig.cmake
+++ cmake/osbaseConfig.cmake
@@ -1,7 +1,9 @@
 get_filename_component(osbase_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 
+list(APPEND CMAKE_MODULE_PATH "${osbase_CMAKE_DIR}/cmake")
 find_dependency(Threads REQUIRED)
+find_dependency(LibRt REQUIRED)
 find_dependency(iotty REQUIRED)
 
 if(NOT TARGET EMBTOM::osbase)
